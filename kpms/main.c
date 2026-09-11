c6bbab69b390c7163e85e999fd989e9690186f99
JI287 <2271712533@qq.com>
2026-09-11 14:37:14 +0800

    Update module description and process listing functionality

--- kpms/main.c
+++ kpms/main.c
@@ -1,17 +1,30 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("test");
-MODULE_DESCRIPTION("test module");
+MODULE_DESCRIPTION("GKI out-of-tree module: list all processes");
+MODULE_VERSION("0.1");
 
-static int __init test_init(void){
-    pr_info("kpms loaded\n");
+static int __init kpms_init(void)
+{
+    struct task_struct *task;
+    pr_info("kpms loaded, start dump process list:\n");
+
+    for_each_process(task) {
+        pr_info("pid:%d | comm:%s\n", task->pid, task->comm);
+    }
+    pr_info("kpms: process list done\n");
     return 0;
 }
-static void __exit test_exit(void){
+
+static void __exit kpms_exit(void)
+{
     pr_info("kpms unloaded\n");
 }
 
-module_init(test_init);
-module_exit(test_exit);
+module_init(kpms_init);
+module_exit(kpms_exit);
